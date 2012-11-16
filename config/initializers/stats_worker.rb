734398092ca4d3b6e3da89d09b736b8818f5fd2a
Benn Stancil <bennstancil@gmail.com>
2012-11-16 03:43:42 -0800

    killed stat worker

--- config/initializers/stats_worker.rb
+++ config/initializers/stats_worker.rb
@@ -1,19 +1,19 @@
-require 'thread'
-
-WorkerThread = Thread.new {
-  loop do
-
-    begin
-      if (active_hack = Hack.where(:active_hack => true).first)
-        hack_score = active_hack.adj_score(active_hack)
-        VoteLog.create(:hack => active_hack, :hack_score => hack_score)
-        puts 1
-      end
-      
-    rescue StandardError => err
-      puts err.message
-    end
-   
-    sleep 5
-  end
-}
+#require 'thread'
+#
+#WorkerThread = Thread.new {
+#  loop do
+#
+#    begin
+#      if (active_hack = Hack.where(:active_hack => true).first)
+#        hack_score = active_hack.adj_score(active_hack)
+#        VoteLog.create(:hack => active_hack, :hack_score => hack_score)
+#        puts 1
+#      end
+#      
+#    rescue StandardError => err
+#      puts err.message
+#    end
+#   
+#    sleep 5
+#  end
+#}
